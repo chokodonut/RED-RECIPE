2bbd553b348dd092a930a8a84442e3067ab4a114
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-04-09 07:26:31 +0000

    add devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  
   resources :recipes do
     resources :comments, only: :create
     collection do
       get 'search'
     end
+  end
+  
+  namespace :admin do
+    root to: 'admin/homes#top'
+    resources :users
   end
   get 'users/my_page' => 'users#show', as: 'my_page'
   get 'my_page/edit' => 'users#edit', as: 'user_edit'
