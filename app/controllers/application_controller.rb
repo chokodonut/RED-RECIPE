61141daca42810cd7f2e8f51f3762507a16200e9
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-06-19 12:31:12 +0000

    admin.follow_fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, if: :admin_url
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
 
   def after_sign_in_path_for(resource)
     root_path
@@ -15,6 +20,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  
-  
+
+
 end
